3685564ca024495c86e425eb267256c5791e31d1
loopadaniel4 <loopadaniel4@gmail.com>
2025-08-25 11:36:05 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,12 +4,12 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2023-06-23', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到MaoSu机场', url: 'https://www.gomaosu.top' },
+    { title: '让全球连接没有界限！', url: 'https://maosu.vip' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,12 +18,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '机场VPN卖点',
+  HEO_HERO_TITLE_2: '稳定、安全、跨境、IEPL 专线',
+  HEO_HERO_TITLE_3: 'www.gomaosu.top',
+  HEO_HERO_TITLE_4: '「支持多平台 · 保护隐私 · 一键即连」',
+  HEO_HERO_TITLE_5: '「IEPL 专线 | 三网优化 | 极速稳定」',
+  HEO_HERO_TITLE_LINK: 'https://gomaosu.top',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
